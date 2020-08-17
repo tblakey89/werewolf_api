55ee9cdbad5fd044138e735994d500b96fc66fdd
Thomas Blakey <tomblakey89@googlemail.com>
2020-08-17 23:18:31 +0100

    Add columns for scheduled games
    Need start at for time the game will start
    
    Type to differentiate between public, scheduled, and private
    
    Closed, if the start at time is passed and no game is started

---
+++ priv/repo/migrations/20200817220610_add_columns_for_scheduled_games.exs
@@ -0,0 +1,11 @@
+defmodule WerewolfApi.Repo.Migrations.AddColumnsForScheduledGames do
+  use Ecto.Migration
+
+  def change do
+    alter table(:games) do
+      add :start_at, :utc_datetime
+      add :type, :string
+      add :closed, :bool
+    end
+  end
+end
